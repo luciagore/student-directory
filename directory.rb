4c7ff074897434e3408fc193779ba7ff67d32a74
luciagore <lucy.gore@hotmail.co.uk>
2018-05-11 12:12:27 +0100

    exercises 5 and 7

--- directory.rb
+++ directory.rb
@@ -1,14 +1,34 @@
 @students = []
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students, followed by their favourite
+   hobby, and cohort".center(50)
+  puts "To finish, just hit return twice".center(50)
+
+  puts "Name: ".center(50)
   name = STDIN.gets.chomp
+  puts "Hobby: ".center(50)
+  hobby = STDIN.gets.chomp
+  puts "Cohort: ".center(50)
+  cohort = STDIN.gets.chomp
+
   while !name.empty? do
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
-    name = gets.chomp
+    cohort = 'n/a' if cohort.empty?
+    hobby = 'n/a' if hobby.empty?
+    push_students(name, hobby, cohort)
+    puts "Now we have #{@students.count} student".center(50) if @students.count == 1
+    puts "Now we have #{@students.count} students".center(50) if @students.count > 1
+    puts "Name: ".center(50)
+    name = STDIN.gets.chomp
+    puts "Hobby: ".center(50)
+    hobby = STDIN.gets.chomp
+    puts "Cohort: ".center(50)
+    cohort = STDIN.gets.chomp
   end
+end
+
+def push_students(name, hobby, cohort)
+  @students << {name: name, hobby: hobby, cohort: cohort.to_sym}
 end
 
 def interactive_menu
@@ -19,11 +39,11 @@ def interactive_menu
 end
 
 def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
-  puts "9. Exit" # 9 because we'll be adding more items
+  puts "1. Input the students".center(50)
+  puts "2. Show the students".center(50)
+  puts "3. Save the list to students.csv".center(50)
+  puts "4. Load the list from students.csv".center(50)
+  puts "9. Exit".center(50) # 9 because we'll be adding more items
 end
 
 def show_students
@@ -45,29 +65,29 @@ def process(selection)
   when "9"
     exit # this will cause the program to terminate
   else
-    puts "I don't know what you meant, try again"
+    puts "I don't know what you meant, try again".center(50)
   end
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(50)
+  puts "-------------".center(50)
 end
 
 def print_student_list
-  puts "enter first letter to filter by"
+  puts "enter first letter to filter by".center(50)
   l = gets.chomp
   i = 0
   while i < @students.length
     if @students[i][:name][0] == l
-      puts "#{i.next}. #{@students[i][:name]} (#{@students[i][:cohort]} cohort)"
+      puts "#{i.next}. #{@students[i][:name]} (hobby is #{@students[i][:hobby]})(#{@students[i][:cohort]} cohort)".center(50)
     end
     i +=1
    end
  end
 
 def print_footer
-  puts "Overall, we have #{@students.count} great students"
+  puts "Overall, we have #{@students.count} great students".center(50)
 end
 
 def save_students
@@ -95,9 +115,9 @@ def try_load_students
   return if filename.nil? # get out of the method if it isn't given
   if File.exists?(filename) # if it exists
     load_students(filename)
-     puts "Loaded #{@students.count} from #{filename}"
+     puts "Loaded #{@students.count} from #{filename}".center(50)
   else # if it doesn't exist
-    puts "Sorry, #{filename} doesn't exist."
+    puts "Sorry, #{filename} doesn't exist.".center(50)
     exit # quit the program
   end
 end
